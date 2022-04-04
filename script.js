8e30c48cd6b7d4e7c75fcc041092a1b54bd1555c
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-04-04 16:14:29 +0200

    fixed the javascript code

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondEl = document.getElementById("second");
 
-const newYear = "1 jan 2021";
-function countdown()
-{
+const newYear = "1 jan 2023";
+function countdown() {
     const newYearDate = new Date(newYear);
     const currentDate = new Date();
 
-    const totalsecond = (newYearDate - currentDate)/1000;
+    const totalsecond = (newYearDate - currentDate) / 1000;
 
-    const days = math.floor(totalsecond/3600/24);
-    const hours = math.floor(totalsecond/3600)%24;
-    const mins= math.floor(totalsecond/60)%60;
-    const second = math.floor(totalsecond)%60;
+    const days = Math.floor(totalsecond / 3600 / 24);
+    const hours = Math.floor(totalsecond / 3600) % 24;
+    const mins = Math.floor(totalsecond / 60) % 60;
+    const second = Math.floor(totalsecond) % 60;
+
+    console.log(days);
 
     daysEl.innerHTML = days;
-    daysEl.innerHTML = formatTime(hours);
-    daysEl.innerHTML = formatTime(mins);
-    daysEl.innerHTML = formatTime(second);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondEl.innerHTML = formatTime(second);
 
 }
-function formatTime(time)
-{
-    return time<10 ? '0${time}':time;
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
 }
 
 countdown();
 
-setInterval(countdown,1000);
+setInterval(countdown, 1000);
